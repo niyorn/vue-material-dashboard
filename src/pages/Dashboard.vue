b388fdc2fa5d613b32650f69c14b06e7a2a225da
Chanakarn Niyornram <c.niyornram@hotmail.com>
2023-01-12 10:51:57 +0100

    Add sidebar

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -1,58 +1,70 @@
 <template>
-  <div class="content">
-      <div style="margin-bottom: 1rem">
-          <span>local url: </span>
-          <input
-              type="text"
-              v-model.trim.lazy="localUrl"
-              placeholder="Project ID"
-          >
+    <div class="content">
+        <div style="margin-bottom: 1rem">
+            <span>local url: </span>
+            <input
+                    v-model.trim.lazy="localUrl"
+                    placeholder="Project ID"
+                    type="text"
+            >
 
-          <span>  -  </span>
+            <span>  -  </span>
 
-          <span>project-id: </span>
+            <span>project-id: </span>
 
-          <input
-              type="number"
-              v-model.trim.lazy.number="projectId"
-              placeholder="Project ID"
-          >
+            <input
+                    v-model.trim.lazy.number="projectId"
+                    placeholder="Project ID"
+                    type="number"
+            >
 
-          <span>  -  </span>
+            <span>  -  </span>
 
-          <span>Constructed Iframe url: </span>
-          <span>
-              <strong>{{localUrl+'/extern/advies?project-id='+projectId}}</strong>
+            <span>Constructed Iframe url: </span>
+            <span>
+              <strong>{{ localUrl + '/extern/advies?project-id=' + projectId }}</strong>
           </span>
-      </div>
+        </div>
 
-    <iframe :src="localUrl+'/extern/advies?project-id='+projectId"
-      frameborder="0"
-      width="100%"
-      height="100%"
-      style="overflow:hidden;height:87vh;width:100%"
-      >
-    </iframe>
-  </div>
+        <iframe :src="localUrl+'/extern/advies?project-id='+projectId"
+                frameborder="0"
+                height="100%"
+                style="overflow:hidden;height:87vh;width:100%"
+                width="100%"
+        >
+        </iframe>
+
+        <div
+                class="sidebar"
+        >
+            <iframe :src="localUrl+'//extern/klant-documenten/verwerk?project-id='+projectId"
+                    frameborder="0"
+                    height="100%"
+                    style="overflow:hidden;height:87vh;width:100%"
+                    width="100%"
+            >
+            </iframe>
+        </div>
+    </div>
 </template>
 
 <script>
-import {
-  StatsCard,
-  ChartCard,
-  NavTabsCard,
-  NavTabsTable,
-  OrderedTable,
-} from "@/components";
 
-export default {
-  components: {
-  },
-  data() {
-    return {
-        localUrl: 'https://adpi.local.adpi.cloud',
-        projectId: 7
+    export default {
+        components: {},
+        data() {
+            return {
+                localUrl: 'https://adpi.local.adpi.cloud',
+                projectId: 7
+            };
+        },
     };
-  },
-};
 </script>
+
+<style lang="scss" scoped>
+    .sidebar {
+      display: flex;
+      flex: 0 0 33.33333333%;
+      max-width: 33.33333333%;
+    }
+</style>
